8a9b398677fd3c99ed51119b90e7beae5b82ad08
Vlad <vlad.herasymovychh@gmail.com>
2025-10-23 09:44:10 +0200

    feat: restructure App component to integrate UserList and Counter with collaborative session data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-function App() {
+import { useCollaborativeSession } from './hooks/useCollaborativeSession';
+
+import Counter from '@/components/Counter/Counter';
+import UserList from '@/components/UserList/UserList';
+
+export default function App() {
+  const { users, counter, updateCounter } = useCollaborativeSession();
+
   return (
-    <>
-      <h1>Collab Dashboard</h1>
-    </>
+    <div>
+      <UserList users={users} />
+      <Counter
+        counter={counter}
+        updateCounter={updateCounter}
+        users={users}
+      />
+    </div>
   );
 }
-
-export default App;
